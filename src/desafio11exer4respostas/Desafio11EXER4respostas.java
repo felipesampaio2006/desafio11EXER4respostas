93c9b8a13f4f519d1a029d986d468c5ba5197211
felipe Sampaio <samlipe2006@gmail.com>
2021-06-10 15:36:21 -0300

    desafio 11 feito dia 10/06/2021

---
+++ src/desafio11exer4respostas/Desafio11EXER4respostas.java
@@ -0,0 +1,18 @@
+package desafio11exer4respostas;
+/**
+ *
+ * @author Felipe
+ */
+public class Desafio11EXER4respostas {
+    public static void main(String[] args) {
+        int cont;
+        cont = 10;
+        while(cont>=+3){
+            if(cont == 3){
+                System.out.println(cont);
+            }
+        }
+        cont--;
+    }
+    
+}
